2bf3b11c51288a543c5e2a3e7bad5a7af9ab36ca
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-11-26 16:35:03 +0000

    add: TestProject and TextEditor

---
+++ TestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(50, 202);
+
+Console.WriteLine($"First Dice roll: {roll1}");
+Console.WriteLine($"Second Dice roll: {roll2}");
+Console.WriteLine($"Third Dice roll: {roll3}");
+
+Console.WriteLine("------------------");
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
