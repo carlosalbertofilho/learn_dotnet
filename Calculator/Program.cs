f420e9c9bf96f037f1362e6b307443b806587e06
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-11-26 02:29:05 +0000

    add: Soma

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O resultado de "+v1+" + "+v2+" é "+(v1+v2));
+        }
+    }
+}
