a63a51a6b0608f2e18fdc852fd7ce38114204da7
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-11-26 15:29:56 +0000

    add: Stopwatch app

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+
+            if (data == "0")
+                System.Environment.Exit(0);
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            Start(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+            int correntTime = 0;
+            while (correntTime <= time)
+            {
+                Console.Clear();
+                Console.WriteLine(correntTime);
+                correntTime++;
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Menu();
+        }
+
+    }
+}
