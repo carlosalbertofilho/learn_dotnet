50737fc8517c4ce0c5b26119063f9bd8f8ac58b7
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-11-26 02:31:56 +0000

    add: Menu e operações

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,6 +6,27 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Selecione uma opção: ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.WriteLine("Opção: ");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1: Add(); break;
+                case 2: Sub(); break;
+                case 3: Mult(); break;
+                case 4: Div(); break;
+                default: ShowError(); break;
+            }
+        }
+
+        static void Add()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -15,6 +36,51 @@ namespace Calculator
             float v2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("O resultado de "+v1+" + "+v2+" é "+(v1+v2));
+        }
+
+        static void Sub()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O resultado de "+v1+" - "+v2+" é "+(v1-v2));
+        }
+
+        static void Mult()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O resultado de "+v1+" * "+v2+" é "+(v1*v2));
+        }
+
+        static void Div()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("O resultado de "+v1+" / "+v2+" é "+(v1/v2));
+        }
+
+        static void ShowError()
+        {
+            Console.Clear();
+            Console.WriteLine("Opção inválida!");
         }
     }
 }
