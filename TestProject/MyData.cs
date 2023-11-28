6473cf9af2a046d9b4c06be9b8234eabfc434f24
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-11-28 17:20:45 +0000

    feat: fix

--- TestProject/MyData.cs
+++ TestProject/MyData.cs
@@ -16,7 +16,16 @@ namespace TestProject
             Console.WriteLine("Usando cultura pt-BR: "+DateTime.Now.ToString("D", pt)); // Retorna a data no formato longo   
             Console.WriteLine("Usando cultura en-US, padrão: "+DateTime.Now.ToString("D", en)); // Retorna a data no formato longo   
 
+            TimeSpan timeSpan = new TimeSpan(2, 14, 18);
 
+            // Exiba a duração total em horas
+            Console.WriteLine("Duração total em horas: " + timeSpan.TotalHours);
+
+            // Exiba a duração total em minutos
+            Console.WriteLine("Duração total em minutos: " + timeSpan.TotalMinutes);
+
+            // Exiba a duração total em segundos
+            Console.WriteLine("Duração total em segundos: " + timeSpan.TotalSeconds);
         }
     }
 }
