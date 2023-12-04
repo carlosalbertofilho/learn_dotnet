1313bda155a7f5d6a37e4acbdc634143abaaf79a
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-12-04 21:00:29 +0000

    feat: Test Program

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Balta.ContentContext;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var articles = new List<Article>
+            {
+                new Article("Artigo sobre OOP", "orientacao-objetos")
+            };
+
+            Console.WriteLine(articles[0].Id);
+            Console.WriteLine(articles[0].Title);
+            Console.WriteLine(articles[0].Url);
+
+            var course =
+                new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseOOP =
+                new Course("OOP em C#", "oop-csharp");
+            var courseAspnet =
+                new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+            var careers =
+                new Career("Especialista .NET", "especialista-dotnet");
+
+            careers.Items.Add(
+                new CareerItem(
+                    1,
+                    "Comece por aqui",
+                    "Fundamentos OOP - I", course));
+            careers.Items.Add(
+                new CareerItem(
+                    2,
+                    "Aprenda OOP",
+                    "Fundamentos OOP - II", courseOOP));
+            careers.Items.Add(
+                new CareerItem(
+                    3,
+                    "Aprenda .NET",
+                    "Fundamentos OOP - III", courseAspnet));
+
+
+            Console.WriteLine(careers.Title);
+            foreach (var career in careers.Items.OrderBy(x => x.Order))
+            {
+                Console.WriteLine($"{career.Order} - {career.Title}");
+                Console.WriteLine(career.Course?.Title);
+            }
+        }
+    }
+}
