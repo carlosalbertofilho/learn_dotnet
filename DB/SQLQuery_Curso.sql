f5811aebb66684064f2c8ddb1a88a72d4a5a4f7a
carlosalberto_filho <carlosalberto_filho@outlook.com>
2024-02-01 17:17:52 -0300

    #DB: Query Curso

--- DB/SQLQuery_Curso.sql
+++ DB/SQLQuery_Curso.sql
@@ -1,10 +1,15 @@
-SELECT TOP 10
-    [Curso].[Id] AS [CursoId], 
-    [Curso].[Nome] AS [Curso], 
-    [Categoria].[Nome] AS [Categoria]
-FROM
-    [Curso]
-    INNER JOIN
-    [Categoria]
-    ON 
+CREATE OR ALTER VIEW vwCursoPorCategoria
+AS
+    SELECT TOP 10
+        [Curso].[Id] AS [CursoId],
+        [Curso].[Nome] AS [Curso],
+        [Categoria].[Nome] AS [Categoria]
+    FROM
+        [Curso]
+        INNER JOIN
+        [Categoria]
+        ON 
     [Curso].[CategoriaId] = [Categoria].[Id]
+    ORDER BY
+    [Curso].[Nome]
+
